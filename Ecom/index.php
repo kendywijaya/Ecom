a72c9b0c086b4f4ecaed5931dfbb5247b324778b
kendywijaya <76420698+kendywijaya@users.noreply.github.com>
2020-12-24 19:46:04 +0700

    Add files via upload

---
+++ Ecom/index.php
@@ -0,0 +1,97 @@
+<?php $hp = [
+    ["Iphone 12 Pro Max", "November, 2020", "Super Retina XDR OLED, 6,7 inci", "Apple A14 Bionic", "Apple GPU (4-core graphics)", "6 GB", "128/256/512 GB", "-", "12 MP + 12 MP + 12 MP + TOF", "12 MP + SL 3D", "Non-removable Li-Ion 3687 mAh"],
+    ["Huawei Mate 40 Pro+", "November, 2020", "OLED 6,76 inci", "Kirin 9000 5G", "Mali-G78 MP24", "12 GB", "256 GB", "Memori Nano hingga 256 GB", "50 MP + 12 MP + 8 MP + 20 MP + TOF", "13 MP + TOF 3D", "Non-removable Li-Po 4400 mAh"],
+    ["Samsung Galaxy Note20 Ultra", "Agustus, 2020", "Dynamic AMOLED, 6.9 inch 1440 x 3088 piksel", "Exynos 990 (7 nm+)", "Mali-G77 MP11", "8 GB", "256 GB, 512 GB", "-", "108 MP + 12 MP + 12 MP", "10 MP", "Non-removable Li-Ion 4,500 mAh"],
+    ["Asus ROG Phone 3", "Juli, 2019", "AMOLED, 6.59 inches", "Qualcomm SM8250 Snapdragon 865+ (7 nm+)", "Adreno 650", "8 GB, 12 GB, 16 GB", "128/8 GB, 128/12 GB, 256/12 GB, 512/12 GB, 512/16 GB", "-", "Triple 64 MP + 13 MP + 5 MP", "24 MP", "Non-removable Li-Po 6000 mAh"],
+    ["Xiaomi Mi 10 Ultra", "Agustus, 2020", "OLED, 6.67 inches", "Qualcomm SM8250 Snapdragon 865 (7 nm+)", "Adreno 650", "8 GB, 12 GB, 16 GB", "128 GB, 256 GB, 512 GB", "-", "Quad 48 MP + 48 MP + 12 MP + 20 MP", "20 MP", "Non-removable Li-Ion 4500 mAh"],
+    ["Sony Xperia 1 II 5G", "Februari, 2020", "OLED, 6,5 inci, 1644 x 3840 piksel", "Qualcomm SM8250 Snapdragon 865", "Adreno 650", "8 GB", "256 GB", "Hingga 1 TB", "12 MP + 12 MP + 12 MP + VGA", "8 MP", "Non-removable Li-Po 4000 mAh"],
+    ["Realme X50 Pro", "Maret, 2020", "Super AMOLED, 6,44 inci", "Snapdragon 865", "Adreno 650", "12 GB", "256 GB", "-", "64 MP + 12 MP + 8 MP + 2 MP", "32 MP + 8 MP", "Non-removable Li-Po 4200 mAh"],
+    ["Oppo Find X2 Pro", "Maret, 2020", "MOLED, 6,7 inci, 1440 x 3168 piksel", "Qualcomm SM8250 Snapdragon 865", "Adreno 650", "12 GB", "521 GB", "-", "48 MP + Periskop 13 MP + 48 MP", "32 MP", "Non-removable Li-Po 4260 mAh"],
+    ["Vivo X50 Pro+", "Juli, 2020", "AMOLED, 6,56 inci", "Qualcomm SM8250 Snapdragon 865 (7 nm+)", "Adreno 650", "8 GB, 12 GB", "128 GB, 256 GB", "-", "50 MP + 8 MP + 13 MP + 8 MP", "32 MP", "Non-removable Li-Po 4135 mAh"],
+    ["LG V60 ThinQ 5G", "Maret, 2020", "P-OLED, 6,8 inci, 1080 x 2460 pixels", "Snapdragon 865 (7 nm+)", "Adreno 650", "8 GB", "128 GB", "microSDXC", "64 MP + 13 MP + VGA", "10 MP", "Non-removable Li-Po 5000 mAh"]
+]; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .bg {
+            background-color: black;
+        }
+
+        .gambar {
+            width: 65%;
+            height: auto;
+            margin: 5px;
+            display: block;
+        }
+    </style>
+</head>
+
+<body class="bg">
+    <h1 align="center">
+        <font color="white">E-Commerce</font>
+    </h1>
+    <table border="1">
+        <tr>
+            <td>
+                <font color="white">Home</font>
+            </td>
+            <td>
+                <font color="white">Media Sosial</font>
+            </td>
+        </tr>
+    </table>
+    <table border="1">
+        <tr align="center" bgcolor="#A9A9A9">
+            <td>SMARTPHONE</td>
+            <td>SPECIFICATION</td>
+        </tr>
+        <tr bgcolor="#808080">
+            <td width="27%" align="center">
+                <img src="pic/iphone12_pro3.jpg" alt="Iphone 12 Pro Max" title="Iphone 12 Pro Max" class="gambar">
+                <img src="pic/huawei_mate40pro.jpg" alt="Huawei Mate 40 Pro+" title="Huawei Mate 40 Pro+" class="gambar">
+                <img src="pic/Samsung_Galaxy_Note20_Ultra_3.jpg" alt="Samsung Galaxy Note20 Ultra" title="Samsung Galaxy Note20 Ultra" class="gambar">
+                <img src="pic/asus_rog_pro3.jpg" alt="Asus ROG Phone 3" title="Asus ROG Phone 3" class="gambar">
+                <img src="pic/Xiaomi_Mi_10_ultra.jfif" alt="Xiaomi Mi 10 Ultra" title="Xiaomi Mi 10 Ultra" class="gambar">
+                <img src="pic/Sony_Xperia_10_II_2.jpg" alt="Sony Xperia 1 II 5G" title="Sony Xperia 1 II 5G" class="gambar">
+                <img src="pic/Realme_X50_Pro_5G_3.jpg" alt="Realme X50 Pro" title="Realme X50 Pro" class="gambar">
+                <img src="pic/Oppo_Find_X2_Pro_1.jpg" alt="Oppo Find X2 Pro" title="Oppo Find X2 Pro" class="gambar">
+                <img src="pic/Vivo_X50_Pro_Plus.jpg" alt="Vivo X50 Pro+" title="Vivo X50 Pro+" class="gambar">
+                <img src="pic/LG_V60_ThinQ_5G_1.jpg" alt="LG V60 ThinQ 5G" title="LG V60 ThinQ 5G" class="gambar">
+            </td>
+            <td>
+                <?php foreach ($hp as $h) : ?>
+                    <ul>
+                        <li>Nama : <?= $h[0] ?></li>
+                        <li>Rilis : <?= $h[1]; ?></li>
+                        <li>Layar : <?= $h[2]; ?></li>
+                        <li>Chipset : <?= $h[3]; ?></li>
+                        <li>GPU : <?= $h[4]; ?></li>
+                        <li>RAM : <?= $h[5]; ?></li>
+                        <li>Memori Internal : <?= $h[6]; ?></li>
+                        <li>Memori Eksternal : <?= $h[7]; ?></li>
+                        <li>Kamera Belakang : <?= $h[8]; ?></li>
+                        <li>Kamera Depan : <?= $h[9]; ?></li>
+                        <li>Baterai : <?= $h[10]; ?></li>
+                    </ul>
+                <?php endforeach; ?>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h3 align="right">
+                    <font color="white">
+                        <marquee behavior="scroll" direction="left">author : aming and kendy</marquee>
+                        </div>
+                </h3>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
